3144824d33e7a59f8aba9fd1cca44a9f603bb528
JILANE009 <golamjilane1@gmail.com>
2024-12-05 17:41:57 +0600

    UI design added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", helpers.Repo.Home)
 	mux.Get("/about", helpers.Repo.About)
 	mux.Get("/reservation", helpers.Repo.Reservation)
+	mux.Post("/reservation", helpers.Repo.PostReservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
